834e90304e3f55ff73e7bc9c7784982900dc8aa7
Mansi Patil <mansipatil056@gmail.com>
2026-04-11 21:33:17 +0530

    STRICT_SYNC: updating module path to server.app:main

--- server/app.py
+++ server/app.py
@@ -58,7 +58,8 @@ def verify(score: float):
     return {"score": score, "valid": verify_score(score)}
 
 def main():
-    uvicorn.run("app:app", host="0.0.0.0", port=7860, reload=False)
+
+    uvicorn.run("server.app:main", host="0.0.0.0", port=7860, reload=False)
 
 if __name__ == "__main__":
     main()
